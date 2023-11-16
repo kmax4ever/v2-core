4035ba8882e770c06f28dfd104286a7c7690a1f9
kmax3ever <kmax3ever2@gmail.com>
2023-11-16 21:42:38 +0700

    init deploy

---
+++ scripts/deploy.ts
@@ -0,0 +1,22 @@
+import { ethers } from 'hardhat'
+
+async function main() {
+  if (!process.env.RPC_ENDPOINT || !process.env.PKEY) {
+    throw 'Missing PRC or PKey!'
+  }
+
+  const [owner] = await ethers.getSigners()
+  var signer = owner
+
+  console.log('xxx signer address', signer.address)
+
+  const UniswapV2Factory = await ethers.getContractFactory('UniswapV2Factory')
+  const uniswapV2Factory = await UniswapV2Factory.deploy(signer.address)
+  var fs = require('fs')
+  fs.writeFileSync('address.json', JSON.stringify({ uniswapV2Factory: uniswapV2Factory.target }, null, 4))
+}
+
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
